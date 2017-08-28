42a8b9cd42dcf6d11fcb6dcb3621041b0b398814
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-08-28 17:08:17 +0100

    greet user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ io.on('connection', (socket) => {
   numberOfConnectedUsers++;
   console.log(`User ${numberOfConnectedUsers} connected`);
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -22,6 +34,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
