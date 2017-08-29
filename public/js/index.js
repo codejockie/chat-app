230537c0ad3c658d1782c074ca214dd725743bad
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-08-29 11:57:21 +0100

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,29 +26,37 @@ socket.on('newLocationMessage', function (message) {
   $('#messages').append(li);
 });
 
+// Get the message input field
+const messageTextbox = $("input[name='message']");
+
 $('#message-form').on('submit', function (event) {
   event.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
-    text: $("input[name='message']").val()
+    text: messageTextbox.val()
   }, function (data) {
-    console.log('Got it', data);
+    messageTextbox.val('');
   });
 });
 
+// Get send location button
 const locationButton = $('#send-location');
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
